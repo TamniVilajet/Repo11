83fd1bfc02f0ca1087af45e397f203c20933e835
TamniVilajet <ermin.kirlic@gmail.com>
2022-04-10 22:57:31 +0200

    Exercise 03

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -2,22 +2,15 @@
 var myPetsArray = ["Dog", "Cat"];
 // End of myPetsArray array
 // Create a function below this line
-function myArrayFunction(myNewPets) {
-    var firstPet;
-    var lastPet;
-    myNewPets.push("Bird");
-    myNewPets.push("Fish");
-    // myNewPets.concat(["Birds", "Fish"]);
+function myArrayFunction(myPets) {
+    var myNewPets = myPetsArray.concat(myPets);
 
-    firstPet = myNewPets[0];
-    lastPet = myNewPets[1];
-
+    var firstPet = myNewPets.shift();
+    var lastPet = myNewPets.pop();
     myNewPets.unshift("Lion");
-    
-    myNewPets.splice(1,1);
-    myNewPets.splice(3,1);
+
     return myNewPets;
 };
-console.log(myArrayFunction(myPetsArray));
 // End of creating a function
-module.exports = myArrayFunction(myPetsArray);
+console.log(myArrayFunction(["Birds", "Fish"]));
+module.exports = myArrayFunction();
